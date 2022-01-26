a9c253e2c40dc98300b354874fc3eb2122d1176f
Pablo <pablo.serranomanzarbeitia@telefonica.com>
2022-01-26 12:22:59 +0100

    Added highlights to siblings

--- src/app/network-graph/data.ts
+++ src/app/network-graph/data.ts
@@ -6,8 +6,9 @@ export const links: any[]  = [];
 const MAIN_NODE_SIZE = 40;
 const CHILD_NODE_SIZE = 15;
 const LEAF_NODE_SIZE = 5;
+
 const MAIN_NODE_DISTANCE = 30;
-const DEFAULT_DISTANCE = 20;
+const DEFAULT_DISTANCE = 50;
 const LEAF_NODE_DISTANCE = 30;
 
 export const MANY_BODY_STRENGTH = -20;
@@ -16,13 +17,19 @@ let i = 0;
 const addMainNode = (node: any) => {
     node.size = MAIN_NODE_SIZE;
     node.color = colors[i++][1];
+    node.collapsing = 0;
+    node.collapsed = false;
+    node.type = 'Main'
     nodes.push(node);
 };
 
-const addChildNode = (parentNode: any, childNode: any, size: any = CHILD_NODE_SIZE, distance: any = DEFAULT_DISTANCE) => {
+const addChildNode = (parentNode: any, childNode: any, size: any = CHILD_NODE_SIZE, distance: any = DEFAULT_DISTANCE, node_type: any = '2nd') => {
     childNode.size = size;
-    nodes.push(childNode);
+    childNode.collapsing = 0;
+    childNode.collapsed = false;
     childNode.color = parentNode.color;
+    childNode.type = node_type
+    nodes.push(childNode);
     links.push({source: parentNode, target: childNode, distance, color: parentNode.color });
 };
 
@@ -31,7 +38,7 @@ const assembleChildNode = (parentNode: any, id: any, numLeafs: number = 20) => {
     addChildNode(parentNode, childNode);
     
     for(let i=0; i<numLeafs; i++){
-        addChildNode(childNode, {id:''}, LEAF_NODE_SIZE, LEAF_NODE_DISTANCE);
+        addChildNode(childNode, { id:'' }, LEAF_NODE_SIZE, LEAF_NODE_DISTANCE, '3rd');
     }
 };
 
@@ -74,12 +81,19 @@ assembleChildNode(ambitioUS, 'Common Future', 0);
 assembleChildNode(ambitioUS, 'FreeLancers Union', 0);
 assembleChildNode(ambitioUS, 'US Federation of Worker Cooperatives', 0);
 
+for(let i=0; i<nodes.length; i++) {
+    nodes[i].collapsing = 0;
+    nodes[i].collapsed = false;
+  }
+
 connectMainNodes(artsWeb, socialImpactCommons);
 connectMainNodes(communityArts, artsWeb);
 connectMainNodes(communityArts, socialImpactCommons);
 connectMainNodes(ambitioUS, socialImpactCommons);
 connectMainNodes(ambitioUS, communityArts);
 connectMainNodes(ambitioUS, artsWeb);
+
+
 
 
 
--- src/app/network-graph/network-graph.component.ts
+++ src/app/network-graph/network-graph.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
-import { forceSimulation, forceLink, forceManyBody, forceCenter, drag } from 'd3';
+import { forceSimulation, forceLink, forceManyBody, forceCenter, drag, zoom } from 'd3';
 import * as d3 from 'd3-selection';
 import { links, nodes, MANY_BODY_STRENGTH } from './data';
 
 @Component({
   selector: 'app-network-graph',
   templateUrl: './network-graph.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class NetworkGraphComponent implements OnInit {
 
@@ -18,8 +17,12 @@ export class NetworkGraphComponent implements OnInit {
 
     const svg = d3.select('#container');
 
-    const centerX = 950/2;
-    const centerY = 500/2;
+    const width = 1500;
+    const height = 1000;
+    const centerX = width/2;
+    const centerY = height/2;
+
+    let linkedByIndex: any = {};
 
     const simulation = forceSimulation(nodes)
       .force('charge', forceManyBody().strength(MANY_BODY_STRENGTH))
@@ -40,6 +43,11 @@ export class NetworkGraphComponent implements OnInit {
       .append('line')
       .attr('stroke', ((link: any) => link.color || 'black') as any);
 
+    for(let link of links){
+      linkedByIndex[link.source.index + "," + link.target.index] = 1;
+      linkedByIndex[link.target.index + "," + link.source.index] = 1;
+    }
+
     const circles = svg
       .selectAll('circle')
       .data(nodes)
@@ -47,8 +55,10 @@ export class NetworkGraphComponent implements OnInit {
       .append('circle')
       .attr('fill', ((node: any) => node.color || 'gray') as any)
       .attr('r', ((node: any) => node.size) as any)
-      .call(dragInteraction);
-
+      .call(dragInteraction)
+      .on("mouseover", mouseover)
+      .on("mouseout", (node:any) => mouseout(node))
+      .on("contextmenu", (node:any) =>  alert(JSON.stringify(node.srcElement.__data__)));
     
     const text = svg
       .selectAll('text')
@@ -59,22 +69,109 @@ export class NetworkGraphComponent implements OnInit {
       .attr('alignment-baseline', ('middle') as any)
       .style('pointer-events', ('none') as any)
       .text( (node: any) => node.id );
-
+    
+    //Legend
+    let legendIconX = 10;
+    let legendIconY = 10;
+    let legendTextX = 20;
+    let legentTextY = 20;
+    for(let node of nodes){
+      if(node.id){
+        svg
+          .append("circle")
+          .attr("cx",legendIconX)
+          .attr("cy",legendIconY)
+          .attr("r", 6)
+          .style("fill", node.color)
+        svg
+          .append("text")
+          .attr("x", legendTextX)
+          .attr("y", legentTextY)
+          .attr("alignment-baseline","middle")
+          .text(node.id)
+          .style("font-size", "15px")
+          .style('pointer-events', ('none') as any)
+        legendIconY = legentTextY += 18;
+      }
+    } 
+    
     simulation.on('tick', () => {
-      circles
-        .attr('cx', ((node: any) => node.x) as any)
-        .attr('cy', ((node: any) => node.y) as any);
-      
-      text
-        .attr('x', ((node: any) => node.x) as any)
-        .attr('y', ((node: any) => node.y) as any);
- 
+      let margin = 20;
+        circles
+          .attr('cx', ((node: any) => Math.max(margin, Math.min(width - margin, node.x))) as any)
+          .attr('cy', ((node: any) => Math.max(margin, Math.min(height - margin, node.y))) as any);
+        
+        text
+          .attr('x', ((node: any) => node.x) as any)
+          .attr('y', ((node: any) => node.y) as any);
+   
+        lines
+          .attr('x1', ((link: any) => link.source.x) as any)
+          .attr('y1', ((link: any) => link.source.y) as any)
+          .attr('x2', ((link: any) => link.target.x) as any)
+          .attr('y2', ((link: any) => link.target.y) as any)
+      });
+
+    function neighboring(a: any, b: any) {
+      return linkedByIndex[a.index + "," + b.index] || a.source.id === b.id;
+    }
+
+    function mouseover(d: any) {
+        lines
+          .attr("stroke","black")
+          .attr("stroke-width",2);
+        lines
+          .transition()
+          .duration(500)
+          .style("opacity", function(o: any) {
+              return o.source.id === d.srcElement.__data__.id || o.target.id === d.srcElement.__data__.id ? 1 : .1;
+          });
+        circles
+          .transition()
+          .duration(500)
+          .style("opacity", function(o: any) {
+            let relatedNodes = getLinks(d.srcElement.__data__);
+            relatedNodes.push(d.srcElement.__data__.id);
+            return relatedNodes.includes(o.id) ? 1 : .1;
+        });
+    }
+
+    function mouseout(d: any) { 
       lines
-        .attr('x1', ((link: any) => link.source.x) as any)
-        .attr('y1', ((link: any) => link.source.y) as any)
-        .attr('x2', ((link: any) => link.target.x) as any)
-        .attr('y2', ((link: any) => link.target.y) as any)
-    });
+        .attr('stroke', ((line: any) => line.color) as any );
+      lines
+        .transition()
+        .duration(500)
+        .style("opacity", 1);
+      circles
+        .attr('fill', ((node: any) => node.color || 'gray') as any)
+        .attr('r', ((node: any) => node.size) as any)
+      circles
+        .transition()
+        .duration(500)
+        .style("opacity", 1);
+     }
+
+   /* function zoomed() {
+      svg.attr("transform", "translate(" +  + ")scale(" + event.scale + ")");
+     }
+*/
+    function getLinks(o: any){
+      const result = [];
+      for(let link of links){
+        if(link.source.id === o.id){
+          result.push(link.target.id);
+        }
+        if(link.target.id === o.id){
+          result.push(link.source.id);
+        }
+      }
+      return result;
+    }
+
+    
   }
+
+  
 
 }
